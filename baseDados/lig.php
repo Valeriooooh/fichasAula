07c1ce031d3b299cf2bb43f6c12708bf3d99f922
Valeriooooh <diogocsvalerio@protonmail.com>
2021-01-11 22:52:37 +0000

    table

--- baseDados/lig.php
+++ baseDados/lig.php
@@ -1,5 +1,17 @@
 <?php
-$con = mysqli_connect("localhost", $_POST["userf"], $_POST["passwdf"], "BD_Valerio");
+$con = mysqli_connect("localhost", "root", "", "BD_Valerio");
 if (!$con) {
     echo "erro";
 }
+$query = mysqli_query($con, "select * from Utilizador");
+echo "<table>
+  <tr>
+    <th>num</th>
+    <th>Nome</th>
+    <th>Email</th>
+    <th>Pass</th>
+  </tr><tr>";
+while ($value = mysqli_fetch_array($query)) {
+    echo "<td>" . $value['Nprocesso'] . "</td><td>" . $value['Nome'] . "</td><td>" . $value['Email'] . "</td><td>" . $value['Senha'] . "</td></tr> ";
+}
+echo "</table>";
